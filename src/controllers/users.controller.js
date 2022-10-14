75264347fe70994793f3081b870087fc153abd08
Clément <clement.ozor@protonmail.com>
2022-10-14 10:39:14 +0200

    [develop] => Add create user

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -1,4 +1,5 @@
 import * as usersService from "../services/users.service.js"
+import {getProps} from "../utils/props.js";
 
 export const getUsers = async (req, res) => {
     let limit = -1
@@ -15,12 +16,47 @@ export const getUsers = async (req, res) => {
     }
 
     try {
-        return await res.status(200).json(usersService.getUsers(limit, skip))
+        return res.status(200).json(await usersService.getUsers(limit, skip))
     } catch (e) {
         console.error(e)
 
         return res.status(500).json({
-            message: e.message,
+            message: e.message
+        })
+    }
+}
+
+export const createUser = async (req, res) => {
+    if (!req.body.discord_identifier ||
+        !req.body.discord_username ||
+        !req.body.discord_discriminator ||
+        !req.body.discord_avatar)
+        return res.status(422).json({
+            message: "Mandatory fields are missing."
+        })
+
+    try {
+        if (await usersService.isUserExists(req.body.discord_identifier))
+            return res.status(409).json({
+                message: `User (${req.body.discord_identifier}) already exists.`
+            })
+
+        try {
+            const data = getProps(req.body, 'discord_identifier', 'discord_username', 'discord_discriminator', 'discord_avatar')
+
+            return res.status(201).json(await usersService.createUser(data))
+        } catch (e) {
+            console.error(e)
+
+            return res.status(500).json({
+                message: e.message,
+            })
+        }
+    } catch (e) {
+        console.error(e)
+
+        return res.status(500).json({
+            message: e.message
         })
     }
 }
--- src/services/users.service.js
+++ src/services/users.service.js
@@ -7,5 +7,21 @@ export const getUsers = async (limit, skip) => {
         return prisma.user.findMany({
             skip: skip,
             take: limit
-        });
+        })
 }
+
+export const isUserExists = async (discord_identifier) => {
+    const user = await prisma.user.findUnique({
+        where: {
+            discord_identifier: discord_identifier
+        }
+    })
+
+    return user !== null
+}
+
+export const createUser = async (data) => (
+    await prisma.user.create({
+        data: data
+    })
+)
