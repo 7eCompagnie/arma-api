5974f4bb590482e1475b0101b1867e3d9f595080
Clément <clement.ozor@protonmail.com>
2022-10-15 12:59:05 +0200

    [develop] => Change order of some variables to handle errors

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -2,20 +2,20 @@ import * as usersService from "../services/users.service.js"
 import {getProps} from "../utils/props.js";
 
 export const getUsers = async (req, res) => {
-    let limit = -1
-    let skip = -1
-
-    if ((req.query.limit != null && req.query.skip == null) ||
-        (req.query.limit == null && req.query.skip != null))
-        return res.status(422).json({
-            message: "Please fill in the two fields \"skip\" and \"limit\"."
-        })
-    else if (req.query.limit != null && req.query.skip != null) {
-        limit = parseInt(req.query.limit)
-        skip = parseInt(req.query.skip)
-    }
-
     try {
+        let limit = -1
+        let skip = -1
+
+        if ((req.query.limit != null && req.query.skip == null) ||
+            (req.query.limit == null && req.query.skip != null))
+            return res.status(422).json({
+                message: "Please fill in the two fields \"skip\" and \"limit\"."
+            })
+        else if (req.query.limit != null && req.query.skip != null) {
+            limit = parseInt(req.query.limit)
+            skip = parseInt(req.query.skip)
+        }
+
         return res.status(200).json(await usersService.getUsers(limit, skip))
     } catch (e) {
         console.error(e)
@@ -53,15 +53,15 @@ export const getUser = async (req, res) => {
 }
 
 export const createUser = async (req, res) => {
-    if (!req.body.discord_identifier ||
-        !req.body.discord_username ||
-        !req.body.discord_discriminator ||
-        !req.body.discord_avatar)
-        return res.status(422).json({
-            message: "Mandatory fields are missing."
-        })
-
     try {
+        if (!req.body.discord_identifier ||
+            !req.body.discord_username ||
+            !req.body.discord_discriminator ||
+            !req.body.discord_avatar)
+            return res.status(422).json({
+                message: "Mandatory fields are missing."
+            })
+
         if (await usersService.getUserByDiscordIdentifier(req.body.discord_identifier))
             return res.status(409).json({
                 message: `User (${req.body.discord_identifier}) already exists.`
@@ -80,12 +80,12 @@ export const createUser = async (req, res) => {
 }
 
 export const updateUser = async (req, res) => {
-    if (Object.keys(req.body).length === 0)
-        return res.status(422).json({
-            message: "Provides at least one value to update a user."
-        })
-
     try {
+        if (Object.keys(req.body).length === 0)
+            return res.status(422).json({
+                message: "Provides at least one value to update a user."
+            })
+
         if (req.query.type === "id") {
             if (!await usersService.getUserById(req.params.id))
                 return res.status(404).json({
